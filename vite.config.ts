062d062c2ffecf6c3628746fe6957d7dd6160249
trisemya <trisemya>
2025-07-21 15:24:37 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import mkcert from 'vite-plugin-mkcert';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/pgs/',
   plugins: [
     react(),
     mkcert(),
@@ -14,6 +15,6 @@ export default defineConfig({
       },
       port: 3000,
       https: true,
-      host: '127.0.0.1',
+      host: '0.0.0.0',
   },
 });
